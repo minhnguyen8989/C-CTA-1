6aa51e8c47711498fa39d6096df8f6293f644f85
Minh Nguyen <122935530+minhnguyen8989@users.noreply.github.com>
2025-07-19 15:02:06 -0500

    completed CTA #1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Fictional person information
+    string firstName = "Minh";
+    string lastName = "Nguyen";
+    string streetAddress = "123 Houston Street";
+    string city = "Houston";
+    int zipCode = 88888;
+
+    // Display the information
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Street Address: " << streetAddress << endl;
+    cout << "City: " << city << endl;
+    cout << "Zip Code: " << zipCode << endl;
+
     return 0;
 }
